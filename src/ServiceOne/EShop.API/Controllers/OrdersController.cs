3021b2213ec0370f372f21ed7da2a97f57c41c7e
muhammad-abdulloh <clevercoderr@gmail.com>
2023-11-30 18:11:52 +0500

    add project foe orders jwt

--- src/ServiceOne/EShop.API/Controllers/OrdersController.cs
+++ src/ServiceOne/EShop.API/Controllers/OrdersController.cs
@@ -1,10 +1,11 @@
 ﻿using EShop.API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EShop.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class OrdersController : ControllerBase
     {
@@ -16,18 +17,27 @@ namespace EShop.API.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "Administrator")]
         public async ValueTask<ActionResult> GetAllOrders()
         {
             return Ok(_eshopDbContext.Orders);
         }
 
         [HttpPost]
+        [Authorize(Roles = "Administrator")]
         public async ValueTask<IActionResult> CreateOrder(Order order)
         {
             await _eshopDbContext.Orders.AddAsync(order);
             await _eshopDbContext.SaveChangesAsync();
 
             return Ok(order);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Administrator,User")]
+        public async ValueTask<ActionResult> GetAllOrdersForUsers()
+        {
+            return Ok(_eshopDbContext.Orders);
         }
     }
 }
--- src/ServiceOne/EShop.API/Program.cs
+++ src/ServiceOne/EShop.API/Program.cs
@@ -1,4 +1,5 @@
 using EShop.API;
+using JWTAuthentificationManager;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCustomJwtAuthentication();
 
 var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var dbName = Environment.GetEnvironmentVariable("DB_NAME");
