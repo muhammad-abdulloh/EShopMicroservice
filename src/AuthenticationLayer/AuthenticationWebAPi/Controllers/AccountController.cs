3918ad7817f89eddb75886d8a28c5bec81ed37ea
muhammad-abdulloh <clevercoderr@gmail.com>
2023-11-30 17:47:23 +0500

    generate token use with gateway

---
+++ src/AuthenticationLayer/AuthenticationWebAPi/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using JWTAuthentificationManager;
+using JWTAuthentificationManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationWebAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JwtTokenHandler _jwtTokenHandler;
+
+        public AccountController(JwtTokenHandler jwtTokenHandler)
+        {
+            _jwtTokenHandler = jwtTokenHandler;
+        }
+
+        [HttpPost]
+        public ActionResult<AuthenticationResponse?> Authenticate([FromBody] AuthenticationRequest authenticationRequest)
+        {
+            var authenticationResponse = _jwtTokenHandler.GenerateJwtToken(authenticationRequest);
+
+            if(authenticationResponse == null) return Unauthorized();
+
+            return authenticationResponse;
+        }
+    }
+}
